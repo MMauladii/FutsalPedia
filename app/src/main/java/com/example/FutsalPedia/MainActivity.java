6dfbd1527eb3e0af0093035568bbb56191c98d9a
MMauladii <114370089+MMauladii@users.noreply.github.com>
2023-06-10 15:40:24 +0700

    detail

--- app/src/main/java/com/example/FutsalPedia/MainActivity.java
+++ app/src/main/java/com/example/FutsalPedia/MainActivity.java
@@ -92,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         binding.fabInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
